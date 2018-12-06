99cd9b5775179e9c8bcc2b0a961318f364c86d39
Danny Y. Huang <crazyideas21@gmail.com>
2018-12-06 02:03:10 -0500

    removed textbox from window

--- src/ui.py
+++ src/ui.py
@@ -12,7 +12,7 @@ def start_main_ui(url, host_state):
 
     window = Tk()
     window.title('Princeton IoT Inspector')
-    window.geometry('600x400')
+    window.geometry('500x300')
     window.attributes("-topmost", True)
     window.grid_columnconfigure(0, weight=1)
 
@@ -28,21 +28,12 @@ def start_main_ui(url, host_state):
     ).grid(column=0, row=1)
 
     host_state.status_text.set(
-        'Currently capturing traffic on your local network.\n'
+        'Initializing...\n'
     )
 
     Label(
         window,
         text="Close this window to stop the capture."
     ).grid(column=0, row=2)
-
-    Label(
-        window,
-        text="\n\nCopy the URL below to view the IoT Inspector report."
-    ).grid(column=0, row=3)
-
-    txt_box = Entry(window, width=len(url), justify='center')
-    txt_box.insert(0, url)
-    txt_box.grid(column=0, row=4)
 
     window.mainloop()
