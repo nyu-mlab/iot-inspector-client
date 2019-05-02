f5dbe4777a7d8aff0bb6a8040f9ffbc345bda93d
Danny Y. Huang <crazyideas21@gmail.com>
2019-05-02 12:45:17 -0400

    Changed the order in which scapy is installed for linux

--- v2-src/linux-install-dependencies.sh
+++ v2-src/linux-install-dependencies.sh
@@ -5,8 +5,7 @@ sudo pip2 install Flask
 sudo pip2 install flask-cors
 sudo pip2 install requests
 sudo pip2 install netaddr
-sudo pip2 install scapy
-sudo pip2 install scapy-http
 sudo pip2 install scapy-ssl_tls
+sudo pip2 install scapy-http
 sudo pip3 install netdisco
 
