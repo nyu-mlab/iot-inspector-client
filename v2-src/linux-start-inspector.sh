f92ddc54dc18bc96bf23f394700b4ad2132904ad
Danny Y. Huang <crazyideas21@gmail.com>
2019-05-02 13:49:12 -0400

    updated linux install script

--- v2-src/linux-start-inspector.sh
+++ v2-src/linux-start-inspector.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+cd ~/princeton-iot-inspector/iot-inspector-client/v2-src/
+
 # Start!
 echo "Starting IoT Inspector..."
 sudo -E python2 start_inspector.py persistent
 
 # Update software
 echo "Updating software..."
-cd ~/princeton-iot-inspector/iot-inspector-client/v2-src/
 git pull
 
 # Install dependencies
