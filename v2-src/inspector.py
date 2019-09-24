9f5e72c90a345c6ea358d5f7c9d599555d53ee14
Deepak Kumar <dkumar11@illinois.edu>
2019-09-24 14:27:46 -0500

    Add powershell NetForwarding

--- v2-src/inspector.py
+++ v2-src/inspector.py
@@ -112,7 +112,7 @@ def enable_ip_forwarding():
     elif os_platform == 'linux':
         cmd = ['sysctl', '-w', 'net.ipv4.ip_forward=1']
     elif os_platform == 'windows':
-        cmd = ['Set-NetIPInterface', '-Fowarding', 'Enabled']
+        cmd = ['powershell', 'Set-NetIPInterface', '-Forwarding', 'Enabled']
 
     assert subprocess.call(cmd) == 0
 
@@ -126,6 +126,6 @@ def disable_ip_forwarding():
     elif os_platform == 'linux':
         cmd = ['sysctl', '-w', 'net.ipv4.ip_forward=0']
     elif os_platform == 'windows':
-        cmd = ['Set-NetIPInterface', '-Fowarding', 'Enabled']
+        cmd = ['powershell', 'Set-NetIPInterface', '-Forwarding', 'Disabled']
 
     assert subprocess.call(cmd) == 0
