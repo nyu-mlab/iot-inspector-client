938425d0d5a53b75fa2bcd46d95997de673622bd
Cody Nicoll <cdnicoll@gmail.com>
2022-06-02 10:24:37 -0700

    Graphql server setup

--- ui/default/README.md
+++ ui/default/README.md
@@ -1,1 +1,7 @@
-npx prisma migrate dev --name init
+introspect your database. Note that this will populate your Prisma schema file with Prisma models that represent your database schema:
+`npx prisma introspect`
+
+
+`npx prisma generate`
+
+`npx prisma studio`
--- ui/default/server/src/schema.ts
+++ ui/default/server/src/schema.ts
@@ -15,21 +15,74 @@ export const typeDefs = gql`
     last_updated_ts: Int!
   }
 
+  type Flow {
+    id: ID
+    device_id: ID!
+    device_port: Int!
+    counterparty_ip: String!
+    counterparty_port: Int!
+    counterparty_hostname: String!
+    counterparty_friendly_name: String!
+    counterparty_country: String!
+    counterparty_is_ad_tracking: Int!
+    counterparty_local_device_id: ID!
+    transport_layer_protocol: String!
+    uses_weak_encryption: Int!
+    ts: Float!
+    ts_mod_60: Float!
+    ts_mod_600: Float!
+    ts_mod_3600: Float!
+    window_size: Float!
+    inbound_byte_count: Int!
+    outbound_byte_count: Int!
+    inbound_packet_count: Int!
+    outbound_packet_count: Int!
+  }
+
   type Query {
     device(device_id: String): Device
     devices: [Device!]!
+    flows: [Flow!]!
+    adsAndTrackerBytes(current_time: Int): [Flow]
   }
 `
 
 export const resolvers = {
   Query: {
     device: (_parent, args: { device_id: string }, context: Context) => {
-      return context.prisma.device.findUnique({
+      return context.prisma.devices.findUnique({
         where: { device_id: args.device_id || undefined },
       })
     },
     devices: (_parent, _args, context: Context) => {
-      return context.prisma.device.findMany()
+      return context.prisma.devices.findMany()
+    },
+    flows: (_parent, _args, context: Context) => {
+      return context.prisma.flows.findMany()
+    },
+    // 1654111436
+    //     SELECT
+    //     SUM(outbound_byte_count) AS Bytes
+    // FROM flows
+    // WHERE
+    //     counterparty_port = 80 AND
+    // f.ts >= [current_time] - 3600.0 * 24.0;
+    //
+    //{ts: greaterThan: 1654111436 - 3600.0 * 24.0 }
+    adsAndTrackerBytes: (
+      _parent,
+      args: { current_time: number },
+      context: Context,
+    ) => {
+      return context.prisma.flows.aggregate({
+        where: {
+          counterparty_port: 80,
+          ts: { gte: (args.current_time || 1654111436) - 3600.0 * 24.0 },
+        },
+        // _sum: {
+        //   outbound_byte_count: 1
+        // }
+      })
     },
   },
 }
