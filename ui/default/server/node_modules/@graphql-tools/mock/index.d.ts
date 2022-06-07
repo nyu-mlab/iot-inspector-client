db7720225baf9c3eb007c9a8894c1f04292dea01
Cody Nicoll <cdnicoll@gmail.com>
2022-06-07 16:28:13 -0700

    removing node_modules

--- ui/default/server/node_modules/@graphql-tools/mock/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './MockStore';
-export * from './addMocksToSchema';
-export * from './mockServer';
-export * from './types';
-export * from './MockList';
-export * from './pagination';
