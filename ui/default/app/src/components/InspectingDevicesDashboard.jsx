9a2796e55dd91c5a060ba81fd6cdb1a331fd35ab
Cody Nicoll <cdnicoll@gmail.com>
2022-06-14 08:13:44 -0700

    Code cleanup

--- ui/default/app/src/components/InspectingDevicesDashboard.jsx
+++ ui/default/app/src/components/InspectingDevicesDashboard.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { HiViewGrid, HiViewList, HiRefresh, HiSearch, HiOutlineArrowSmDown } from "react-icons/hi";
-import { BiSortAlt2 } from "react-icons/bi";
-import RefreshSpinner from "./graphics/RefreshSpinner";
-import DeviceItem from "./DeviceItem";
+import React, { useState } from 'react'
+import {
+  HiViewGrid,
+  HiViewList,
+  HiSearch,
+} from 'react-icons/hi'
+import { BiSortAlt2 } from 'react-icons/bi'
+import RefreshSpinner from './graphics/RefreshSpinner'
+import DeviceItem from './DeviceItem'
 import { Switch } from '@headlessui/react'
-import { gql, useQuery } from '@apollo/client';
-import useIntervalQuery from '../hooks/useIntervalQuery';
-
+import { gql } from '@apollo/client'
+import useIntervalQuery from '../hooks/useIntervalQuery'
 
 const DEVICES_QUERY = gql`
   query Query {
@@ -22,13 +25,11 @@ const DEVICES_QUERY = gql`
 
 const InspectingDevicesDashboard = () => {
   const [cardView, setCardView] = useState(false)
-  const devicesResponse = useIntervalQuery(DEVICES_QUERY)
+  const devicesResponse = useIntervalQuery(DEVICES_QUERY, null, 5000)
 
-  const [searchValue, setSearchValue] = useState("")
-
+  const [searchValue, setSearchValue] = useState('')
 
   return (
-
     <section className="bg-gray-50 flex-flex-col-gap-4" id="inspecting-devices">
       <div className="flex items-center w-full gap-4 md:gap-5">
         <div className="">
@@ -40,70 +41,66 @@ const InspectingDevicesDashboard = () => {
         </div>
       </div>
 
-    <div className="grid grid-cols-4 gap-4 py-4 md:flex md:items-center">
-      <form className="flex flex-1 order-last col-span-4 md:order-first">
-        <input
-          type='text'
-          name="search"
-          id='searchDevices'
-          value={searchValue}
-          onChange={e => setSearchValue(e.target.value)}
-          className="w-full px-4 py-2 text-gray-600 bg-white border border-gray-400 rounded-md"
-          placeholder="Search devices by name or tag"/>
-        <label htmlFor="searchDevices" className="sr-only"><HiSearch />Search devices by name or tag</label>
-      </form>
-      {/* <div className="flex items-center justify-center gap-1 p-2 text-sm text-white bg-gray-500 rounded-lg">
-        Tags <HiOutlineArrowSmDown />
-      </div> */}
-      <button className="flex items-center justify-center gap-1 p-2 text-sm">
-        Name
-        <BiSortAlt2 className="w-4 h-4 text-gray-400" />
-      </button>
-      <button className="flex items-center justify-center gap-1 p-2 text-sm">
-        Traffic
-        <BiSortAlt2 className="w-4 h-4 text-gray-400"/>
-      </button>
-      <div className="flex items-center justify-center gap-3 px-2">
-        <Switch
-          checked={cardView}
-          onChange={setCardView}
-          >
-          <span className="flex">
-            <HiViewGrid className={`${
-              cardView ? 'text-white rounded-lg bg-secondary' : 'text-dark'
-            } w-10 h-10 md:w-8 md:h-8 p-1 `}/>
-            <HiViewList className={`${
-              cardView ? 'text-dark' : 'text-white rounded-lg bg-secondary'
-            } w-10 h-10 md:w-8 md:h-8 p-1 `}/>
-          </span>
-        </Switch>
+      <div className="grid grid-cols-4 gap-4 py-4 md:flex md:items-center">
+        <form className="flex flex-1 order-last col-span-4 md:order-first">
+          <input
+            type="text"
+            name="search"
+            id="searchDevices"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="w-full px-4 py-2 text-gray-600 bg-white border border-gray-400 rounded-md"
+            placeholder="Search devices by name or tag"
+          />
+          <label htmlFor="searchDevices" className="sr-only">
+            <HiSearch />
+            Search devices by name or tag
+          </label>
+        </form>
+        <button className="flex items-center justify-center gap-1 p-2 text-sm">
+          Name
+          <BiSortAlt2 className="w-4 h-4 text-gray-400" />
+        </button>
+        <button className="flex items-center justify-center gap-1 p-2 text-sm">
+          Traffic
+          <BiSortAlt2 className="w-4 h-4 text-gray-400" />
+        </button>
+        <div className="flex items-center justify-center gap-3 px-2">
+          <Switch checked={cardView} onChange={setCardView}>
+            <span className="flex">
+              <HiViewGrid
+                className={`${
+                  cardView ? 'text-white rounded-lg bg-secondary' : 'text-dark'
+                } w-10 h-10 md:w-8 md:h-8 p-1 `}
+              />
+              <HiViewList
+                className={`${
+                  cardView ? 'text-dark' : 'text-white rounded-lg bg-secondary'
+                } w-10 h-10 md:w-8 md:h-8 p-1 `}
+              />
+            </span>
+          </Switch>
+        </div>
       </div>
-    </div>
-    <ul className={cardView ? 'card-grid' : 'min-h-[200px]'}>
-
-
-    {devicesResponse?.data?.devices
-
-      .filter( device => {
-        if (!searchValue) return true
-        if (device.auto_name.toLowerCase().includes(searchValue.toLowerCase())) {
-          return true
-        }
-      })
-      .map(device => (
-        <li key={device.device_id} className={`${cardView ? 'card-view' : 'list-view'
-          } py-2`}>
-          <DeviceItem device={device} />
-        </li>
-      ))
-    }
-    {/* {devicesResponse?.data?.devices?.map((device) => (
-      <li key={device.device_id} className={`${cardView ? 'card-view' : 'list-view'
-          } py-2`}>
-        <DeviceItem device={device} />
-      </li>
-    ))} */}
-    </ul>
+      <ul className={cardView ? 'card-grid' : 'min-h-[200px]'}>
+        {devicesResponse?.data?.devices
+          .filter((device) => {
+            if (!searchValue) return true
+            if (
+              device.auto_name.toLowerCase().includes(searchValue.toLowerCase())
+            ) {
+              return true
+            }
+          })
+          .map((device) => (
+            <li
+              key={device.device_id}
+              className={`${cardView ? 'card-view' : 'list-view'} py-2`}
+            >
+              <DeviceItem device={device} />
+            </li>
+          ))}
+      </ul>
     </section>
   )
 }
