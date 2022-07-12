a2544ffdc258b42e81442e74361138ec6315be40
Nathaniel Fostan <nathanielfostan@gmail.com>
2022-07-12 14:13:01 -0500

    existing drawers #25

--- ui/default/app/src/App.jsx
+++ ui/default/app/src/App.jsx
@@ -1,13 +1,11 @@
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
-import EndpointDrawer from './components/EndpointDrawer'
 import Router from './pages'
 
 function App() {
   return (
     <DefaultLayout>
       <div className="App">
-        <EndpointDrawer />
         <main className="flex-1 md:pr-64 lg:md:pr-80">
           <div className="">
             <Router />
