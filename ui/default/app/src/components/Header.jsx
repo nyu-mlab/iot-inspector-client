91df97b587037a7e16e1dffd9d45ff19d7ed08de
cdnicoll <cdnicoll@gmail.com>
2022-07-21 08:28:43 -0700

    Update consent screen / home flow #32

--- ui/default/app/src/components/Header.jsx
+++ ui/default/app/src/components/Header.jsx
@@ -7,88 +7,89 @@ import { Dialog, Tab, Switch, Disclosure, Menu, Transition } from '@headlessui/r
 import SettingsModal from "./header/SettingsModal";
 import AchievmentsModal from "./header/AchievmentsModal";
 import FAQModal from "./header/FAQModal";
+import useUserConfigs from '@hooks/useUserConfigs'
 
 const Header = () => {
   const [settingsOpen, setSettingsOpen] = useState(false)
-
+  const { userConfigsData, userConfigsDataLoading } = useUserConfigs()
 
   return (
     <header className="header">
       <Disclosure as="nav" className="primary-nav">
         {({ open }) => (
           <>
-        <div className="flex justify-between p-6 grow md:px-8 lg:px-12">
-          <a href="/" className="flex gap-2 font-semibold h2 text-dark">
-              <Logo /> Home Data Inspector
-          </a>
-          <div className="flex items-center gap-4">
+            <div className="flex justify-between p-6 grow md:px-8 lg:px-12">
+              <a href={userConfigsData?.userConfigs?.is_consent == 1 ? '/overview' : '/'} className="flex gap-2 font-semibold h2 text-dark">
+                <Logo /> Home Data Inspector
+              </a>
+              <div className="flex items-center gap-4">
 
-            <div className="hidden gap-2 md:flex">
-              <AnalyzingTraffic />
-            </div>
+                <div className="hidden gap-2 md:flex">
+                  <AnalyzingTraffic />
+                </div>
 
-            <div className="flex lg:hidden">
-              <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-secondary/80 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
-                <span className="sr-only">Open main menu</span>
-                {open ? (
-                  <HiX className="block w-6 h-6" aria-hidden="true" />
-                ) : (
-                  <HiMenu className="block w-6 h-6" aria-hidden="true" />
-                )}
-              </Disclosure.Button>
+                <div className="flex lg:hidden">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-secondary/80 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <HiX className="block w-6 h-6" aria-hidden="true" />
+                    ) : (
+                      <HiMenu className="block w-6 h-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
             </div>
-        </div>
-        </div>
-        <div className="hidden gap-8 p-6 text-white lg:flex bg-dark">
-          <DataToggle />
-          {/*
+            <div className="hidden gap-8 p-6 text-white lg:flex bg-dark">
+              <DataToggle />
+              {/*
           TODO: Notifications
           <button>
             <HiBell className="w-6 h-6" />
           </button> */}
-          <button onClick={() => setSettingsOpen(true)}>
-            <HiCog className="w-6 h-6 transition hover:rotate-180 hover:text-primary" />
-          </button>
-        </div>
-
-
-        {/* Responsive Navigation */}
-        <Disclosure.Panel className="lg:hidden">
-            <div className="px-4 pt-2 pb-3 space-y-1">
-              <Disclosure.Button
-                as="a"
-                href="/"
-                className="block pb-6 text-xl text-dark"
-              >
-                Network Dashboard
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="a"
-                href="/communication-endpoints/"
-                className="block pb-6 text-xl text-dark"
-              >
-                Communication Endpoints
-              </Disclosure.Button>
-              <Disclosure.Button
-                as="button"
-                href="/communication-endpoints/"
-                className="block pb-6 text-xl font-semibold text-dark"
-                onClick={() => setSettingsOpen(true)}
-              >
-                Settings
-              </Disclosure.Button>
-            </div>
-            <div className="px-4 py-6 bg-dark lg:hidden">
-              <DataToggle />
+              <button onClick={() => setSettingsOpen(true)}>
+                <HiCog className="w-6 h-6 transition hover:rotate-180 hover:text-primary" />
+              </button>
             </div>
 
-            <hr />
-            <div className="px-4 py-6 md:hidden">
-              <AnalyzingTraffic />
-            </div>
-        </Disclosure.Panel>
-        </>
-      )}
+
+            {/* Responsive Navigation */}
+            <Disclosure.Panel className="lg:hidden">
+              <div className="px-4 pt-2 pb-3 space-y-1">
+                <Disclosure.Button
+                  as="a"
+                  href="/"
+                  className="block pb-6 text-xl text-dark"
+                >
+                  Network Dashboard
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="a"
+                  href="/communication-endpoints/"
+                  className="block pb-6 text-xl text-dark"
+                >
+                  Communication Endpoints
+                </Disclosure.Button>
+                <Disclosure.Button
+                  as="button"
+                  href="/communication-endpoints/"
+                  className="block pb-6 text-xl font-semibold text-dark"
+                  onClick={() => setSettingsOpen(true)}
+                >
+                  Settings
+                </Disclosure.Button>
+              </div>
+              <div className="px-4 py-6 bg-dark lg:hidden">
+                <DataToggle />
+              </div>
+
+              <hr />
+              <div className="px-4 py-6 md:hidden">
+                <AnalyzingTraffic />
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
       </Disclosure>
       <Transition show={settingsOpen} as={Fragment}>
         <Dialog
@@ -105,7 +106,7 @@ const Header = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-          <div className="modal-backdrop" aria-hidden="true" />
+            <div className="modal-backdrop" aria-hidden="true" />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
@@ -116,58 +117,58 @@ const Header = () => {
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
           >
-          <div className="inspector-settings">
-            <Dialog.Panel className="inspector-settings-panel">
-              <Tab.Group >
-                <Tab.List className="tab-nav">
-                  <Tab as={Fragment}>
-                    {({ selected }) => (
-                    <button
-                        className={
-                          selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
-                        }
-                      >
-                      Settings
-                    </button>
-                    )}
-                  </Tab>
-                  <Tab as={Fragment}>
-                    {({ selected }) => (
-                    <button
-                        className={
-                          selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
-                        }
-                      >
-                      Achievments
-                    </button>
-                    )}
-                  </Tab>
-                  <Tab as={Fragment}>
-                    {({ selected }) => (
-                    <button
-                        className={
-                          selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
-                        }
-                      >
-                      FAQs
-                    </button>
-                    )}
-                  </Tab>
-                </Tab.List>
-                <Tab.Panels className="px-8 py-6 md:py-8 md:px-12">
-                  <Tab.Panel>
-                    <SettingsModal />
-                  </Tab.Panel>
-                  <Tab.Panel >
-                    <AchievmentsModal />
-                  </Tab.Panel>
-                  <Tab.Panel>
-                    <FAQModal />
-                  </Tab.Panel>
-                </Tab.Panels>
-              </Tab.Group>
-            </Dialog.Panel>
-          </div>
+            <div className="inspector-settings">
+              <Dialog.Panel className="inspector-settings-panel">
+                <Tab.Group >
+                  <Tab.List className="tab-nav">
+                    <Tab as={Fragment}>
+                      {({ selected }) => (
+                        <button
+                          className={
+                            selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
+                          }
+                        >
+                          Settings
+                        </button>
+                      )}
+                    </Tab>
+                    <Tab as={Fragment}>
+                      {({ selected }) => (
+                        <button
+                          className={
+                            selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
+                          }
+                        >
+                          Achievments
+                        </button>
+                      )}
+                    </Tab>
+                    <Tab as={Fragment}>
+                      {({ selected }) => (
+                        <button
+                          className={
+                            selected ? 'tab-nav-item tab-nav-item-active' : 'tab-nav-item'
+                          }
+                        >
+                          FAQs
+                        </button>
+                      )}
+                    </Tab>
+                  </Tab.List>
+                  <Tab.Panels className="px-8 py-6 md:py-8 md:px-12">
+                    <Tab.Panel>
+                      <SettingsModal />
+                    </Tab.Panel>
+                    <Tab.Panel >
+                      <AchievmentsModal />
+                    </Tab.Panel>
+                    <Tab.Panel>
+                      <FAQModal />
+                    </Tab.Panel>
+                  </Tab.Panels>
+                </Tab.Group>
+              </Dialog.Panel>
+            </div>
           </Transition.Child>
         </Dialog>
       </Transition>
