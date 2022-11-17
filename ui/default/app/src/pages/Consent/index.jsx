9acda48473fb666158be5378ac1421e6bc83e7f1
cdnicoll <cdnicoll@gmail.com>
2022-11-17 12:57:58 -0800

    Adding API for consent

--- ui/default/app/src/pages/Consent/index.jsx
+++ ui/default/app/src/pages/Consent/index.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import useUserConfigs from '@hooks/useUserConfigs'
+import ReactHtmlParser from 'react-html-parser'
+
 import NoConsentLayout from '../../layouts/NoConsentLayout'
+import useCopy from '@hooks/useCopy'
 
 const Consent = () => {
-  const { userConfigsData, userConfigsDataLoading, updateUserConfigs } =
-    useUserConfigs()
-  const [canAutoInspectDevice, setCanAutoInspectDevice] = useState(true)
+  const { userConfigsDataLoading, updateUserConfigs } = useUserConfigs()
   const navigate = useNavigate()
+
+  const { loading, data } = useCopy('/consent.json')
 
   if (userConfigsDataLoading) {
     return ''
@@ -30,53 +33,7 @@ const Consent = () => {
             <div className='flex flex-col items-center justify-center max-w-2xl gap-8 mx-auto text-center h-fit'>
               <h1>Consent Statement</h1>
               <div className='overflow-scroll h-72 md:px-8 '>
-                <p>
-                  We may share some of your information with outside
-                  researchers. We will remove your name, contact information,
-                  and any other information that identifies you personally.
-                  <a href=''>FAQ</a>
-                </p>
-                <p>
-                  We will remove your name, contact information (excluding ZIP
-                  code), and any other information that identifies you
-                  personally from any data we share with outside researchers.
-                </p>
-                <p>
-                  Consumer Reports conducts research studies with consumers to
-                  understand their experiences with products and services they
-                  use and their views on various consumer issues. Your responses
-                  will be used to produce information designed to help consumers
-                  in today's marketplace, to move the marketplace in ways that
-                  protect and empower consumers, and to support engagement and
-                  advocacy efforts to influence legislators, regulatory bodies,
-                  and other marketplace actors.
-                </p>
-                <p>
-                  This Internet of Things (IoT) study consists of 1) downloading
-                  the HDI app and 2) running it in a browser window to better
-                  understand how the devices in your home connect to the
-                  internet, and identify potential security and privacy
-                  violations.
-                </p>
-                <p>
-                  We may share aggregated findings in an anonymized way at
-                  Consumer Reports and NYU, so that our researchers can identify
-                  security and privacy violations to keep consumers better
-                  protected in the IoT space. We will remove your name, contact
-                  information and any other information that might identify you
-                  personally from any aggregated questionnaire that we share
-                  with third parties.
-                </p>
-                <p>
-                  We may follow up with you as part of our research and
-                  reporting. We will not use any information that identifies you
-                  in our reporting unless we contact you and you specifically
-                  give us permission.
-                </p>
-                <p>
-                  Learn more about this project.
-                  <a href=''>Read our FAQs</a>
-                </p>
+                {data && ReactHtmlParser(data.body)}
               </div>
               <hr className='w-full md:w-9/12' />
               <button className='btn btn-primary' onClick={handleSubmit}>
