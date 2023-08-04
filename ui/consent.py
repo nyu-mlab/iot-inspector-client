5cbebd0ecc4a91781605df41ad72071c20373db0
Danny Y. Huang <crazyideas21@gmail.com>
2023-08-04 13:17:41 -0400

    rewrote consent logic to show a two-step consent process

--- ui/consent.py
+++ ui/consent.py
@@ -51,32 +51,55 @@ def show_data_donation_consent():
     config.set('has_consented_to_data_donation', 'not_set')
 
     c1, c2, c3 = st.columns([0.7, 0.15, 0.15])
-    c1.markdown('I want to give consent to participate in this study.')
-    yes_donate_with_survey = c2.button('Yes', 'yes_donate_with_survey')
-    no_donate_with_survey = c3.button('No', 'no_donate_with_survey')
+    c1.markdown('I want to give consent to participate in this study, which includes\n * donating my network traffic data to the IoT Inspector project\n * receiving personalized feedback from IoT Inspector\n * taking a survey about my experience with IoT Inspector')
+    c2.button('Yes', 'yes_donate_with_survey', use_container_width=True, type='primary', on_click=yes_donate_with_survey_callback)
+    c3.button('No', 'no_donate_with_survey', use_container_width=True, on_click=no_donate_with_survey_callback)
 
-    st.divider()
+    if 'show_second_consent' not in st.session_state:
+        st.session_state['show_second_consent'] = False
 
-    c1, c2, c3 = st.columns([0.7, 0.15, 0.15])
-    c1.markdown('I give consent to data donation and receiving personalized feedback from the IoT Inspector, but not taking the survey.')
-    yes_donate = c2.button('Yes', 'yes_donate')
-    no_donate = c3.button('No', 'no_donate')
+    if st.session_state['show_second_consent']:
+        st.divider()
+        c1, c2, c3 = st.columns([0.7, 0.15, 0.15])
+        c1.markdown('I give consent to data donation and receiving personalized feedback from IoT Inspector, but not taking the survey.')
+        c2.button('Yes', 'yes_donate', use_container_width=True, on_click=yes_donate_callback)
+        c3.button('No', 'no_donate', use_container_width=True, on_click=no_donate_callback)
+        st.session_state['show_second_consent'] = False
 
-    if yes_donate_with_survey:
-        config.set('has_consented_to_data_donation', 'donation_with_survey')
 
-    if yes_donate:
-        config.set('has_consented_to_data_donation', 'donation_only')
 
-    if yes_donate or yes_donate_with_survey:
-        config.set('should_donate_data', True)
-        config.set('donation_start_ts', int(time.time()))
-        st.experimental_rerun()
+def yes_donate_with_survey_callback():
 
-    if no_donate_with_survey or no_donate:
-        config.set('has_consented_to_data_donation', 'no_consent')
-        config.set('should_donate_data', False)
-        st.experimental_rerun()
+    config.set('has_consented_to_data_donation', 'donation_with_survey')
+    yes_callback()
+
+
+
+def yes_donate_callback():
+
+    config.set('has_consented_to_data_donation', 'donation_only')
+    yes_callback()
+
+
+
+def yes_callback():
+    config.set('should_donate_data', True)
+    config.set('donation_start_ts', int(time.time()))
+
+
+
+def no_donate_with_survey_callback():
+
+    st.session_state['show_second_consent'] = True
+
+
+
+def no_donate_callback():
+
+    config.set('has_consented_to_data_donation', 'no_consent')
+    config.set('should_donate_data', False)
+    config.set('donation_start_ts', 0)
+
 
 
 def get_current_file_directory():
