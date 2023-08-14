b486fb3838e90913d30f72e5b6e074368fb62f4f
Danny Y. Huang <crazyideas21@gmail.com>
2023-08-14 16:39:37 -0400

    add prompts to install npcap

--- ui/template.py
+++ ui/template.py
@@ -2,6 +2,40 @@ import sys
 sys.path.insert(0, '../')
 
 import streamlit as st
+import signal
+import time
+
+
+# Make sure that npcap is installed for windows
+from core.common import get_os
+import os
+if get_os() == 'windows':
+    npcap_path = os.path.join(os.environ['WINDIR'], 'System32', 'Npcap')
+    if not os.path.isdir(npcap_path):
+        # Show an error message
+        print('\n' * 20)
+        print('Please install Npcap from https://npcap.com/dist/npcap-1.76.exe before using IoT Inspector.')
+        st.markdown('## Error starting IoT Inspector')
+        st.error('Please install Npcap from https://npcap.com/dist/npcap-1.76.exe before using IoT Inspector.')
+        st.markdown('Npcap is necessary for IoT Inspector to capture and analyze network traffic.')
+        st.markdown('Once you install Npcap, please restart IoT Inspector.')
+
+        my_bar = st.progress(1.0, text='IoT Inspector will close in 30 seconds')
+
+        for t in range(30):
+            time.sleep(1)
+            time_left = 30 - t
+            my_bar.progress(time_left / 30.0, text=f'IoT Inspector will automatically close in {time_left} seconds')
+
+        # Get the current process ID
+        pid = os.getpid()
+
+        # Kill the process
+        os.kill(pid, signal.SIGTERM)
+
+        st.stop()
+
+
 import core.start
 
 core.start.start_threads()
