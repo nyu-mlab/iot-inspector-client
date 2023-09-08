e5b88d3ef14f55448b2d33abf27d1b4b56f945a8
VitoZCY <VitoZCY@outlook.com>
2023-09-08 21:47:25 +0800

    done modification banner grab V1

--- core/banner_grabber.py
+++ core/banner_grabber.py
@@ -26,10 +26,12 @@ RECONNECT_SUCCESS = 1
 RECONNECT_EXCEPTION = 2
 OTHER_EXCEPTION = 3
 
+ASYNC_CONCURRENCY = 10
+
 # Set minimal scan interval to avoid too frequent scan
 BANNER_GRAB_INTERVAL = 120
 # {mac1-port1:scan_time1, mac1-port2:scan_time2} 
-scan_status_record = {}
+last_scan_time_record = {}
 
 
 def generate_random_string(length):
@@ -38,6 +40,8 @@ def generate_random_string(length):
 
 
 async def talk_to_target(ip, port, sock, timeout = 5.0, step = "Unknown", req="", recv=False):
+    """ Send request and receive response through socket """
+
     try:
         # If request message is given, send request to targets
         if req != "":
@@ -55,7 +59,7 @@ async def talk_to_target(ip, port, sock, timeout = 5.0, step = "Unknown", req=""
             return CONNECT_SUCCESS, "Build TCP connection success"
 
     except Exception as e:
-        # If Exception is ConnectionResetError or BrokenPipeError, we try to rebuild the TCP connection
+        # We try to rebuild the TCP connection if the exception is ConnectionResetError or BrokenPipeError
         if isinstance(e, ConnectionResetError) or isinstance(e, BrokenPipeError):
             try:
                 await asyncio.sleep(timeout)
@@ -69,10 +73,11 @@ async def talk_to_target(ip, port, sock, timeout = 5.0, step = "Unknown", req=""
         return OTHER_EXCEPTION, f"{type(e).__name__}"
 
 
-async def async_banner_grab_task(target, timeout=5.0):
+async def async_banner_grab_task(target_ip_port, timeout=5.0):
+    """ Banner grab on one ip:port """
 
     # Get ip and port
-    ip, port = target
+    ip, port = target_ip_port
     if isinstance(port, str):
         port = int(port)
 
@@ -115,12 +120,13 @@ async def async_banner_grab_task(target, timeout=5.0):
     return this_result
 
 
-async def async_banner_grab_tasks(target_list):
+async def async_banner_grab_tasks(target_ip_port_list):
+    """ Control different async banner tasks"""
 
     # Create a list of coroutines for banner grabbing from the given IP and Port lists
     coroutines = []
-    for i in range(0, len(target_list)): # here is one-to-one, not double loop!
-        coro = async_banner_grab_task(target_list[i])
+    for i in range(0, len(target_ip_port_list)): # here is one-to-one, not double loop!
+        coro = async_banner_grab_task(target_ip_port_list[i])
         coroutines.append(coro)
 
     # Wait for all coroutines to complete and get the results
@@ -129,25 +135,28 @@ async def async_banner_grab_tasks(target_list):
     return results
 
 
-def banner_grab(target_list):
-    # potential risk: does not have concurrency level control.
+def banner_grab(target_ip_port_list):
+    """ Launch banner grab """
 
-    if(len(target_list) == 0):
+    if(len(target_ip_port_list) == 0):
         common.log("[Banner Grab] No target to grab for this device")
         return []
 
     common.log('[Banner Grab] Start Banner Grab on {} target {}'.format(
-        len(target_list),
-        ', '.join(str(target) for target in target_list)
+        len(target_ip_port_list),
+        ', '.join(str(target) for target in target_ip_port_list)
     ))
 
-    results = asyncio.run(async_banner_grab_tasks(target_list))
+    # Since one device is unlikely to have too many open TCP ports
+    # The concurrency level is equal to the number of open TCP ports
+    result = asyncio.run(async_banner_grab_tasks(target_ip_port_list))
 
     common.log("[Banner Grab] Done")
-    return results
+    return result
 
 
 def get_current_devices():
+    """ Get known valid devices """
 
     target_device_list = []
     criteria = (model.Device.is_inspected == 1) & (model.Device.ip_addr != '')
@@ -160,6 +169,7 @@ def get_current_devices():
 
 
 def build_ip_port_list(device, target_port_list):
+    """ Build target ip:port list for a given device """
 
     target_ip_port_list = []
 
@@ -172,22 +182,23 @@ def build_ip_port_list(device, target_port_list):
             ports = eval(device.open_tcp_ports)
             for port in ports:
 
-                if (device.mac_addr+"-"+str(port)) in scan_status_record:
-                    if time.time() - scan_status_record[(device.mac_addr+"-"+str(port))] < BANNER_GRAB_INTERVAL:
+                if (device.mac_addr+"-"+str(port)) in last_scan_time_record:
+                    if time.time() - last_scan_time_record[(device.mac_addr+"-"+str(port))] < BANNER_GRAB_INTERVAL:
                         common.log(f"[Banner Grab] give up too frequent scan {device.mac_addr} {ip}:{str(port)}")
                         continue
 
                 target_ip_port_list.append((ip, port))
-                scan_status_record[(device.mac_addr+"-"+str(port))] = time.time()
+                last_scan_time_record[(device.mac_addr+"-"+str(port))] = time.time()
     else:
         for port in target_port_list:
             target_ip_port_list.append((ip, port))
-            scan_status_record[(device.mac_addr+"-"+str(port))] = time.time()
+            last_scan_time_record[(device.mac_addr+"-"+str(port))] = time.time()
 
     return target_ip_port_list
 
 
 def store_result_to_database(device, result):
+    """ Store banner grab result for one device """
 
     # Aggregate raw results into dict
     this_result = {}
@@ -212,11 +223,9 @@ def store_result_to_database(device, result):
             common.log(f"[Banner Grab] Store to database IP:{device.ip_addr} Banners:{device.port_banners}")
 
 
-def run_banner_grab(target_device_list = None, target_ports_list = None): # target_ports_list is List[List], each list for each device
-    """
-    Banner grab on device's open tcp ports
+def run_banner_grab(target_device_list = None, target_ports_list = None): 
+    """ Main API for Banner grab on device's open tcp ports """
 
-    """
     if not global_state.is_inspecting:
         return
 
@@ -224,7 +233,8 @@ def run_banner_grab(target_device_list = None, target_ports_list = None): # targ
     if not config.get('has_consented_to_overall_risks', False):
         return
 
-    # Check arguements. port_list and device_list must be one-to-one relation
+    # target_ports_list is List[List], each list for each device
+    # port_list and device_list must be one-to-one relation
     if target_ports_list != None:
         if (target_device_list == None) or (len(target_device_list) != len(target_ports_list)):
             common.log("[Banner Grab] Args not qualified!")
