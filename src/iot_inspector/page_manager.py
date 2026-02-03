895aee4b694fc5b26486cccb18e8faa537c98efa
Md Jakaria <jakaria.dcc11@gmail.com>
2026-02-03 10:45:09 -0500

    implemented predict-event ml models for event inference

--- src/iot_inspector/page_manager.py
+++ src/iot_inspector/page_manager.py
@@ -21,6 +21,7 @@ from event_detection import burst_processor
 from event_detection import feature_generation
 from event_detection import feature_standardization
 from event_detection import periodic_filter
+from event_detection import predict_event
 
 
 def get_page(title, material_icon, show_page_func):
@@ -107,6 +108,10 @@ def start_inspector_once():
 
     # Start the periodic filter thread
     safe_loop.SafeLoopThread(periodic_filter.start)
+
+    # Start the event prediction thread
+    safe_loop.SafeLoopThread(predict_event.start)
+
 
 
     with st.spinner("Starting Inspector Core Library..."):
